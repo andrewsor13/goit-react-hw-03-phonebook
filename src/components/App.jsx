d6d62a42a8d9c364ccf47960a121ccdf0da20bdf
andrewsor13 <ciobanca.andreisorin@gmail.com>
2023-10-10 21:52:19 +0300

    upp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,8 +22,8 @@ export default function App() {
   };
   const [state, setState] = useState(data);
 
-  if (state.contacts > 4) {
-    setState({ divHeight: state.contacts.length * 60 + 60 });
+  if (state.contacts.length > 4) {
+    setState({ divHeight: state.contacts.length * 60 });
   }
 
   useEffect(() => {
