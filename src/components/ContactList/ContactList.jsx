0927db683b7ecc66ba3bdb36b118b84341671760
andrewsor13 <ciobanca.andreisorin@gmail.com>
2023-10-11 06:29:45 +0300

    fix2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,25 +4,27 @@ import ContactItem from '../ContactItem/ContactItem';
 import styles from './ContactList.module.css';
 
 export default function ContactList({ contacts, filter, onDeleteContact }) {
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  if (contacts) {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
 
-  return (
-    <ul className={styles.contact_list}>
-      {filteredContacts.map(contact => (
-        <li key={contact.id}>
-          <ContactItem contact={contact} />
-          <button
-            className={styles.button}
-            onClick={() => onDeleteContact(contact.id)}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+      <ul className={styles.contact_list}>
+        {filteredContacts.map(contact => (
+          <li key={contact.id}>
+            <ContactItem contact={contact} />
+            <button
+              className={styles.button}
+              onClick={() => onDeleteContact(contact.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 ContactList.propTypes = {
